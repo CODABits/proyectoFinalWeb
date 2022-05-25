f877ddb042bc8f6d1839fedb7bf8f7e4aa329607
Práctica Clínica Umanizales <93500932+CODABits@users.noreply.github.com>
2022-05-25 15:28:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let popup = document.getElementById("popup");
+
+      function abrirPopup(){
+
+        popup.classList.add("open-popup");
+      }
+      
+      function cerrarPopup(){
+        
+        popup.classList.remove("open-popup");
+        
+      }
